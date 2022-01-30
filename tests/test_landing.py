71a3985a2729530e10ef69c17b6f4fe3cf5c0efd
saidgc <said.g@hotmail.com>
2022-01-29 20:00:06 -0600

    feat: Add tests for landing.

---
+++ tests/test_landing.py
@@ -0,0 +1,11 @@
+from http import HTTPStatus
+
+from django.test import TestCase
+
+
+class TestLanding(TestCase):
+
+    def test_view_landing(self):
+        response = self.client.get('/', follow=True)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'landing.html')
